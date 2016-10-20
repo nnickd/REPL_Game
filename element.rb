e64da3b5e19059c1ba73d6e6249b682cff55da78
Nicolas Dellitalia <nicolas.dellitalia@gmail.com>
2016-10-20 12:04:44 -0400

    removed stuff

--- element.rb
+++ element.rb
@@ -3,30 +3,18 @@ require 'matrix'
 class Element
   attr_accessor :kind, :xy, :speed, :limit
 
-<<<<<<< HEAD
   def initialize(kind = '~', xy = [0, 0], speed = [0, 0], limit = 2) # creates element
-=======
-  def initialize(kind = '~', xy = [0, 0], speed = [0, 0], limit = 3) # creates element
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     @kind = kind
     @xy = Vector.elements(xy)
     @speed = Vector.elements(speed)
     @limit = limit
   end
 
-<<<<<<< HEAD
   # moves forward 1 unit of time and makes sure speed doesnt go over the limit
   def tick
     @speed = @speed.normalize * @limit if @speed.magnitude > @limit
     @speed = Vector[@speed[0].round(0), @speed[1].round(0)]
     @xy += @speed
-=======
-  def tick # moves forward 1 unit of time and makes sure speed doesnt go over the limit
-    @speed = @speed.normalize * @limit if @speed.magnitude > @limit
-    @speed = Vector[@speed[0].round(0), @speed[1].round(0)]
-    @xy += @speed
-    @speed *= 0
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
   end
 
   def force(power) # adds a force vector to speed
@@ -44,12 +32,8 @@ class Element
     end
   end
 
-<<<<<<< HEAD
   # wraps player from left to right makes sure not to fall through ground
   def bounds(length)
-=======
-  def bounds(length) # wraps player from left to right makes sure not to fall through ground
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     if @xy[0] < 0
       @xy = Vector[length, @xy[1]]
     elsif @xy[0] > length
@@ -59,19 +43,11 @@ class Element
     end
   end
 
-<<<<<<< HEAD
   def to(x, y, length) # gets an element at a given position relative to calling element
     bound_check = Vector[@xy[0] + x, @xy[1] + y]
     if bound_check[0] < 0
       Vector[length, bound_check[1]]
     elsif bound_check[0] > length - 1
-=======
-  def to(x, y) # gets an element at a given position relative to calling element and wraps if pointing out of bounds
-    bound_check = Vector[@xy[0] + x, @xy[1] + y]
-    if bound_check[0] < 0
-      Vector[15, bound_check[1]]
-    elsif bound_check[0] > 15
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
       Vector[0, bound_check[1]]
     else
       bound_check
@@ -81,7 +57,6 @@ class Element
   def to_be # returns where the element will be next tick
     @xy + @speed
   end
-<<<<<<< HEAD
 
   def collide_next_tick?(other)
     to_be == other.to_be
@@ -108,6 +83,4 @@ class Element
   def nearest_element(elements)
 
   end
-=======
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
 end
--- render.rb
+++ render.rb
@@ -25,19 +25,12 @@ class Render
       puts view[y].join(' ')
     end
     hud
-<<<<<<< HEAD
     @space.debug
-=======
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
   end
 
   def hud # puts stats, controls, and how to quit below game
     return unless @space.hud
-<<<<<<< HEAD
     first = "max height: #{@space.max_height}  |  coins: #{@space.coins}  |  time: #{@space.time}"
-=======
-    first = "max height: #{@space.max_height}  |  time: #{@space.time}"
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     second = "'a' = move left  |  'w' = jump  |  'd' = move right"
     third = "'q' = quit game  |  'h' = toggle hud"
     puts '-' * first.length
@@ -59,46 +52,29 @@ class Render
     command
   end
 
-<<<<<<< HEAD
   # processess user input for moving player, jumping and toggling the hud
   def process_input(command)
     case command.downcase
-=======
-  def process_input(command) # processess user input for moving player, jumping and toggling the hud
-    case command
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     when 'a' # left
       @space.player.force(Vector[-1, 0])
     when 'd' # jump
       @space.player.force(Vector[1, 0])
     when 'w' # up
       @space.player.force(Vector[0, 4]) if @space.jump?
-<<<<<<< HEAD
     when 's' # down
       @space.player.force(Vector[0, -1])
-=======
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     when 'h' # hud
       @space.hud = !@space.hud
     end
   end
 
-<<<<<<< HEAD
   # while the player is still alive, tick space forward, draw game that follows player, and process user input
   def game_loop
-=======
-  def game_loop # while the player is still alive, tick space forward, draw game that follows player, and process user input
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     while @space.alive == true
       @space.tick
       render(@space.length, [@space.player.xy[1] - 5, @space.player.xy[1] + @space.height - 5])
       input = user_input
-<<<<<<< HEAD
       input == 'q' ? break : process_input(input)
-=======
-      break if input == 'q'
-      process_input(input)
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
     end
     you_lose
   end
@@ -110,12 +86,8 @@ class Render
   end
 end
 
-<<<<<<< HEAD
 puts 'length?'
 length = gets.chomp.to_i
 space = Space.new(length, 64, [0, -1])
-=======
-space = Space.new(16, 64, [0, -1])
->>>>>>> 63e23774686169cf08c87ce868cfcf5d4285d6ee
 view = Render.new(space)
 view.game_loop
